b4a82ad19c5ff57168b8a5da998d108e5426297f
Jakub Kozubek <jakub.kozubek@gmail.com>
2021-10-31 21:46:33 +0100

    Added get_next_token into the header file, comented the header file, edited test makefile, added stuff for future scanner test

--- scanner.c
+++ scanner.c
@@ -8,16 +8,15 @@
 
 int get_next_token(T_token *token)
 {
-
     (void)token;
-
-    size_t lineSize = 0;
-    char *line;
-    size_t len = 0;
-    lineSize = getline(&line, &len, stdin);
-    if (lineSize == 0)
-        return -1;
-    (void)lineSize;
-    free(line);
-    return 0;
+    return 1;
+    /* size_t lineSize = 0;
+     char *line;
+     size_t len = 0;
+     lineSize = getline(&line, &len, stdin);
+     if (lineSize == 0)
+         return -1;
+     (void)lineSize;
+     free(line);
+     return 0;*/
 }

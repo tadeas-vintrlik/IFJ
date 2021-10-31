0c39b166faf214ab2a7d1a926e6bd4c40366e5e4
Jakub Kozubek <jakub.kozubek@gmail.com>
2021-10-31 21:46:33 +0100

    Added get_next_token into the header file, comented the header file, edited test makefile, added stuff for future scanner test

--- scanner.h
+++ scanner.h
@@ -1,12 +1,26 @@
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
+/**
+ * @brief Enum for different types of tokens.
+ */
 typedef enum { ID, KEYWORD, DOUBLE } token_type;
 
+/**
+ * @brief Structure for storing a token
+ */
 typedef struct Token {
     token_type type;
     char *value;
     int line;
 } * T_token;
-
+/**
+ * @brief Main function of scanner, returns a token
+ * as a structure, returns a error code TODO
+ *
+ * @param[in/out] token Token structure for storing information about tokens.
+ *
+ * @return int Error value TODO.
+ */
+int get_next_token(T_token *token);
 #endif

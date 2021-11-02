35fe3718b9ac8fb496dd418e9ef83e6ba65d0b34
Kryštof Albrecht <k@nymeria.tech>
2021-11-02 20:50:14 +0100

    Implemented the scanner.

--- scanner.h
+++ scanner.h
@@ -1,17 +1,61 @@
-#ifndef _COMMON_H_
-#define _COMMON_H_
+#include "common.h"
+
+#ifndef _SCANNER_H_
+#define _SCANNER_H_
 
 /**
  * @brief Enum for different types of tokens.
  */
-typedef enum { ID, KEYWORD, DOUBLE, LEFT_PAR, RIGHT_PAR, STRING, COMMA } token_type;
+typedef enum {
+    TOKEN_ID,
+    TOKEN_KEYWORD,
+    TOKEN_INT,
+    TOKEN_NUMBER,
+    TOKEN_STRING,
+    TOKEN_EQUAL,
+    TOKEN_DECLAR,
+    TOKEN_LESS_THAN,
+    TOKEN_LESS_EQUAL_THAN,
+    TOKEN_GREATER_THAN,
+    TOKEN_GREATER_EQUAL_THAN,
+    TOKEN_DIVISION,
+    TOKEN_FLOOR_DIVISION,
+    TOKEN_MUL,
+    TOKEN_SUB,
+    TOKEN_NOT_EQUAL_TO,
+    TOKEN_ADD,
+    TOKEN_STRING_LENGTH,
+    TOKEN_LEFT_BRACKET,
+    TOKEN_RIGHT_BRACKET,
+    TOKEN_COMMA,
+    TOKEN_COLON,
+    TOKEN_EOF
+} token_type;
+
+typedef enum {
+    DO,
+    ELSE,
+    END,
+    FUNCTION,
+    GLOBAL,
+    IF,
+    INTEGER,
+    LOCAL,
+    NIL,
+    NUMBER,
+    REQUIRE,
+    RETURN,
+    STRING,
+    THEN,
+    WHILE
+} Keyword;
 
 /**
  * @brief Structure for storing a token
  */
 typedef struct Token {
     token_type type;
-    char *value;
+    dynamic_string_s *value;
     int line;
 } T_token;
 /**

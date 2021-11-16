0c8b607de00695e22bdccfa35657e3dbe51aedd7
tadeas-vintrlik <xvintr04@stud.fit.vutbr.cz>
2021-11-16 17:00:13 +0100

    add precedence table

--- exp_parser.c
+++ exp_parser.c
@@ -6,10 +6,103 @@
 
 #include "exp_parser.h"
 
-avl_node_s *exp_parse(void)
+/**
+ * @brief Enumeration of different values found inside the precendece table.
+ */
+typedef enum op {
+    /* Table heading */
+    LEN,
+    MULT_DIV,
+    ADD_SUB,
+    CONCAT,
+    REL,
+    LPAR,
+    RPAR,
+    ID,
+    DOLLAR,
+    /* Actions inside the table */
+    PUSH, /* < */
+    RULE, /* > */
+    SPEC, /* = */
+    ERR, /* x */
+    NONE /* Empty table slot */
+} op_e;
+
+/**
+ * @brief Precendence table.
+ */
+op_e table[10][10] = {
+    {NONE,      LEN,  MULT_DIV, ADD_SUB, CONCAT, REL,  LPAR, RPAR, ID,  DOLLAR},
+    {LEN,       RULE, RULE,     RULE,    RULE,   RULE, PUSH, ERR,  PUSH, ERR},
+    {MULT_DIV,  PUSH, RULE,     RULE,    RULE,   RULE, PUSH, RULE, PUSH, RULE},
+    {ADD_SUB,   PUSH, PUSH,     RULE,    RULE,   RULE, PUSH, RULE, PUSH, RULE},
+    {CONCAT,    PUSH, PUSH,     PUSH,    PUSH,   RULE, PUSH, RULE, PUSH, RULE},
+    {REL,       PUSH, PUSH,     PUSH,    PUSH,   ERR,  PUSH, RULE, PUSH, RULE},
+    {LPAR,      PUSH, PUSH,     PUSH,    PUSH,   PUSH, PUSH, SPEC, PUSH, ERR},
+    {RPAR,      ERR,  RULE,     RULE,    RULE,   RULE, ERR,  RULE, ERR,  RULE},
+    {ID,        ERR,  RULE,     RULE,    RULE,   RULE, PUSH, RULE, ERR,  RULE},
+    {DOLLAR,    ERR,  PUSH,     PUSH,    PUSH,   PUSH, PUSH, ERR,  PUSH, NONE},
+};
+
+/**
+ * @brief Returns op_e value of  token op_e searchable in the precedence table.
+ *
+ * @param token The token get op_e value.
+ *
+ * @return op_e value.
+ */
+static op_e token2op(const T_token *token)
+{
+    switch (token->type) {
+        case TOKEN_STRING_LENGTH:
+            return LEN;
+
+        case TOKEN_MUL:
+            return MULT_DIV;
+        case TOKEN_DIVISION:
+            return MULT_DIV;
+        case TOKEN_FLOOR_DIVISION:
+            return MULT_DIV;
+
+        case TOKEN_ADD:
+            return ADD_SUB;
+        case TOKEN_SUB:
+            return ADD_SUB;
+
+        /* TODO: TOKEN_STRING_CONCAT? */
+
+        case TOKEN_LESS_THAN:
+            return REL;
+        case TOKEN_LESS_EQUAL_THAN:
+            return REL;
+        case TOKEN_GREATER_THAN:
+            return REL;
+        case TOKEN_GREATER_EQUAL_THAN:
+            return REL;
+        case TOKEN_EQUAL:
+            return REL;
+        case TOKEN_NOT_EQUAL_TO:
+            return REL;
+
+        case TOKEN_LEFT_BRACKET:
+            return LPAR;
+
+        case TOKEN_RIGHT_BRACKET:
+            return RPAR;
+
+        case TOKEN_ID:
+            return ID;
+
+        default:
+            return DOLLAR;
+    }
+}
+
+avl_node_s *exp_parse(symtable_s *symtable)
 {
     avl_node_s *node;
     avl_init(&node);
 
+    (void)symtable;
     return node;
 }
--- exp_parser.h
+++ exp_parser.h
@@ -8,12 +8,16 @@
 #define _EXP_PARSER_H
 
 #include "avl.h"
+#include "scanner.h"
+#include "symtable.h"
 
 /**
  * @brief Parses expression. Creates Abstract Syntatic Tree.
  *
+ * @param[in] symtable Table of symbols to search for identifiers.
+ *
  * @return Syntactic tree. NULL on invalid expression syntax.
  */
-avl_node_s *exp_parse(void);
+avl_node_s *exp_parse(symtable_s *symtable);
 
 #endif /* _EXP_PARSER_H */

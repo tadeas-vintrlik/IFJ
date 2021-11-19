61a51f981302785d41328eea1669a07f60d30f0a
tadeas-vintrlik <xvintr04@stud.fit.vutbr.cz>
2021-11-19 19:51:16 +0100

    format and author

--- exp_parser.c
+++ exp_parser.c
@@ -1,5 +1,6 @@
 /**
  * @file exp_parser.c
+ * @author Jakub Kozubek <xkozub07@stud.fit.vutbr.cz>
  * @author Tadeas Vintrlik <xvintr04@stud.fit.vutbr.cz>
  * @brief Implementation of bottom-up parser for expressions.
  */
@@ -11,8 +12,7 @@
 /**
  * @brief Enumeration of different values found inside the precendece table.
  */
-typedef enum op
-{
+typedef enum op {
     /* Table heading */
     LEN,
     MULT_DIV,
@@ -27,24 +27,24 @@ typedef enum op
     PUSH, /* < */
     RULE, /* > */
     SPEC, /* = */
-    ERR,  /* x */
-    NONE  /* Empty table slot */
+    ERR, /* x */
+    NONE /* Empty table slot */
 } op_e;
 
 /**
  * @brief Precendence table.
  */
 op_e table[TABLE_ELEM][TABLE_ELEM] = {
-    {NONE, LEN, MULT_DIV, ADD_SUB, CONCAT, REL, LPAR, RPAR, ID, DOLLAR},
-    {LEN, RULE, RULE, RULE, RULE, RULE, PUSH, ERR, PUSH, ERR},
-    {MULT_DIV, PUSH, RULE, RULE, RULE, RULE, PUSH, RULE, PUSH, RULE},
-    {ADD_SUB, PUSH, PUSH, RULE, RULE, RULE, PUSH, RULE, PUSH, RULE},
-    {CONCAT, PUSH, PUSH, PUSH, PUSH, RULE, PUSH, RULE, PUSH, RULE},
-    {REL, PUSH, PUSH, PUSH, PUSH, ERR, PUSH, RULE, PUSH, RULE},
-    {LPAR, PUSH, PUSH, PUSH, PUSH, PUSH, PUSH, SPEC, PUSH, ERR},
-    {RPAR, ERR, RULE, RULE, RULE, RULE, ERR, RULE, ERR, RULE},
-    {ID, ERR, RULE, RULE, RULE, RULE, PUSH, RULE, ERR, RULE},
-    {DOLLAR, ERR, PUSH, PUSH, PUSH, PUSH, PUSH, ERR, PUSH, ERR},
+    { NONE, LEN, MULT_DIV, ADD_SUB, CONCAT, REL, LPAR, RPAR, ID, DOLLAR },
+    { LEN, RULE, RULE, RULE, RULE, RULE, PUSH, ERR, PUSH, ERR },
+    { MULT_DIV, PUSH, RULE, RULE, RULE, RULE, PUSH, RULE, PUSH, RULE },
+    { ADD_SUB, PUSH, PUSH, RULE, RULE, RULE, PUSH, RULE, PUSH, RULE },
+    { CONCAT, PUSH, PUSH, PUSH, PUSH, RULE, PUSH, RULE, PUSH, RULE },
+    { REL, PUSH, PUSH, PUSH, PUSH, ERR, PUSH, RULE, PUSH, RULE },
+    { LPAR, PUSH, PUSH, PUSH, PUSH, PUSH, PUSH, SPEC, PUSH, ERR },
+    { RPAR, ERR, RULE, RULE, RULE, RULE, ERR, RULE, ERR, RULE },
+    { ID, ERR, RULE, RULE, RULE, RULE, PUSH, RULE, ERR, RULE },
+    { DOLLAR, ERR, PUSH, PUSH, PUSH, PUSH, PUSH, ERR, PUSH, ERR },
 };
 
 /**
@@ -56,8 +56,7 @@ op_e table[TABLE_ELEM][TABLE_ELEM] = {
  */
 static op_e token2op(const T_token *token)
 {
-    switch (token->type)
-    {
+    switch (token->type) {
     case TOKEN_STRING_LENGTH:
         return LEN;
 
@@ -133,25 +132,20 @@ op_e table_get_action(op_e in, op_e stack)
     int row = -1, col = -1;
 
     /* First find they key in columns */
-    for (unsigned i = 0; i < TABLE_ELEM; i++)
-    {
-        if (table[0][in] == in)
-        {
+    for (unsigned i = 0; i < TABLE_ELEM; i++) {
+        if (table[0][in] == in) {
             col = i;
         }
     }
 
     /* Then find the key in rows */
-    for (unsigned i = 0; i < TABLE_ELEM; i++)
-    {
-        if (table[i][0] == stack)
-        {
+    for (unsigned i = 0; i < TABLE_ELEM; i++) {
+        if (table[i][0] == stack) {
             row = i;
         }
     }
 
-    if (row != -1 && col != -1)
-    {
+    if (row != -1 && col != -1) {
         ret = table[row][col];
     }
 
@@ -160,14 +154,13 @@ op_e table_get_action(op_e in, op_e stack)
 static bool term2expr(tstack_s *tstack, tstack_s *help)
 {
     T_token *non_terminal;
-    if (!tstack_empty(help))
-    {
-        //TODO possibly free stack
+    if (!tstack_empty(help)) {
+        // TODO: possibly free stack
         return false;
     }
     non_terminal = create_non_terminal();
     tstack_push(tstack, non_terminal);
-    // TODO CODE_GEN call
+    // TODO: CODE_GEN call
     return true;
 }
 
@@ -178,87 +171,74 @@ static bool apply_rule(tstack_s *tstack)
 
     tstack_init(&help);
 
-    while (!tstack_empty(tstack))
-    {
+    while (!tstack_empty(tstack)) {
         tmp = tstack_top(tstack);
 
         tstack_push(&help, tmp);
         tstack_pop(tstack, false);
-        if (tmp->type == TOKEN_HANDLE)
-        {
+        if (tmp->type == TOKEN_HANDLE) {
             break;
         }
     }
     tmp = tstack_top(&help);
 
-    if (!tmp || (tmp && tmp->type != TOKEN_HANDLE))
-    {
+    if (!tmp || (tmp && tmp->type != TOKEN_HANDLE)) {
         return false;
     }
 
     tstack_pop(&help, false);
 
-    switch (tmp->type)
-    {
+    switch (tmp->type) {
     case TOKEN_STRING_LENGTH:
         tmp = tstack_top(&help);
-        if (tmp->type != TOKEN_NON_TERMINAL)
-        {
+        if (tmp->type != TOKEN_NON_TERMINAL) {
             return false;
         }
         tstack_pop(&help, false);
-        //TODO code gen call
+        // TODO: code gen call
         break;
     case TOKEN_LEFT_BRACKET:
         tmp = tstack_top(&help);
-        if (tmp->type != TOKEN_NON_TERMINAL)
-        {
+        if (tmp->type != TOKEN_NON_TERMINAL) {
             return false;
         }
         tstack_pop(&help, false);
         tmp = tstack_top(&help);
-        if (tmp->type != TOKEN_RIGHT_BRACKET)
-        {
+        if (tmp->type != TOKEN_RIGHT_BRACKET) {
             return false;
         }
         tstack_pop(&help, false);
         break;
     case TOKEN_KEYWORD:
-        if (strcmp(tmp->value->content, "nil"))
-        {
+        if (strcmp(tmp->value->content, "nil")) {
             return false;
         }
-        if (!term2expr(tstack, &help))
-        {
+        if (!term2expr(tstack, &help)) {
             return false;
         }
         break;
     case TOKEN_ID:
-        if (!term2expr(tstack, &help))
-        {
+        if (!term2expr(tstack, &help)) {
             return false;
         }
         break;
     case TOKEN_NUMBER:
-        if (!term2expr(tstack, &help))
-        {
+        if (!term2expr(tstack, &help)) {
             return false;
         }
         break;
     case TOKEN_INT:
-        if (!term2expr(tstack, &help))
-        {
+        if (!term2expr(tstack, &help)) {
             return false;
         }
         break;
     case TOKEN_STRING:
-        if (!term2expr(tstack, &help))
-        {
+        if (!term2expr(tstack, &help)) {
             return false;
         }
         break;
     case TOKEN_NON_TERMINAL:
-        //TODO continue here
+        // TODO: continue here
         break;
 
     default:

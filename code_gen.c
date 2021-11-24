302e78a5e7b98a692bbf156d31059ce64e9f97f1
tadeas-vintrlik <xvintr04@stud.fit.vutbr.cz>
2021-11-24 17:51:13 +0100

    finished function generation

--- code_gen.c
+++ code_gen.c
@@ -5,9 +5,9 @@
  * @brief Implementation of code generator.
  */
 
- #include "code_gen.h"
+#include "code_gen.h"
 
- static unsigned counter = 0;
+static unsigned counter = 0;
 
 void gen_prog_start(void)
 {
@@ -17,22 +17,17 @@ void gen_prog_start(void)
     puts("DEFVAR GF@%%tmp2");
 }
 
-void gen_func_start(const char *func_name, tstack_s *in_params, unsigned no_returns)
+/**
+ * @brief  Move input parameters to their actual variable names.
+ *
+ * @param in_params Stack of all the parameters of the function.
+ */
+static void gen_pop_arg(tstack_s *in_params)
 {
     T_token *token;
     unsigned i;
 
-    /* Create label for calls to jump to and PUSH TF to LF */
-    printf("LABEL $-%s\n", func_name);
-    puts("PUSHFRAME");
-
-    /* Create variables for return values */
-    for (i = 0; i < no_returns; i++) {
-        printf("DEFVAR LF@%%retval%d\n", no_returns);
-        printf("MOVE LF@%%retval%d nil@nil\n", no_returns);
-    }
-
-    /* Move input parameters to their actual variable names */
+    /* */
     i = 0;
     while (!tstack_empty(in_params)) {
         token = tstack_top(in_params);
@@ -44,12 +39,29 @@ void gen_func_start(const char *func_name, tstack_s *in_params, unsigned no_retu
     }
 }
 
+void gen_func_start(const char *func_name, tstack_s *in_params, unsigned no_returns)
+{
+    unsigned i = 0;
+
+    /* Create label for calls to jump to and PUSH TF to LF */
+    printf("LABEL $-%s\n", func_name);
+    puts("PUSHFRAME");
+
+    /* Create variables for return values */
+    for (i = 0; i < no_returns; i++) {
+        printf("DEFVAR LF@%%retval%d\n", no_returns);
+        printf("MOVE LF@%%retval%d nil@nil\n", no_returns);
+    }
+    gen_pop_arg(in_params);
+}
+
 /**
- * @brief Helper function for gen_func_end. Moves all the return values to correct out parameters in LF.
+ * @brief Helper function for gen_func_end. Moves all the return values to correct out parameters in
+ * LF.
  *
  * @param return_vals The stack of return value tokens the function should return.
  */
-static void gen_push_arg(tstack_s *return_vals)
+static void gen_push_ret(tstack_s *return_vals)
 {
     T_token *token;
     unsigned i = 0;
@@ -86,16 +98,12 @@ static void gen_push_arg(tstack_s *return_vals)
 void gen_func_end(tstack_s *return_vals)
 {
     /* Move correct return values variables in LF */
-    gen_push_arg(return_vals);
+    gen_push_ret(return_vals);
 
     /* Push current LF as TF for caller to pop the return values and return to addres */
     puts("POPFRAME");
     puts("RETURN");
 }
-
-static void gen_pop_arg();
-static void gen_push_ret();
-static void gen_pop_ret();
 
 unsigned gen_jump_else(void)
 {
@@ -112,10 +120,7 @@ void gen_else_label(unsigned label_number)
     printf("LABEL $-else%d\n", label_number);
 }
 
-void gen_if_end(unsigned label_number)
-{
-    printf("LABEL $-end%d\n", label_number);
-}
+void gen_if_end(unsigned label_number) { printf("LABEL $-end%d\n", label_number); }
 
 unsigned gen_while_label(void)
 {
@@ -131,7 +136,46 @@ void gen_while_jump_end(unsigned label_number)
     printf("JUMPIFNEQ $-end%d GF@%%tmp1 bool@true\n", label_number);
 }
 
-void gen_while_end_label(unsigned label_number)
+void gen_while_end_label(unsigned label_number) { printf("LABEL $-end%d\n", label_number); }
+
+static void gen_push_arg(tstack_s *in_params)
 {
-    printf("LABEL $-end%d\n", label_number);
+    T_token *token;
+    unsigned i = 0;
+
+    /* Move all in paramters values to correct variables */
+    while (!tstack_empty(in_params)) {
+        token = tstack_top(in_params);
+        tstack_pop(in_params, false);
+        printf("DEFVAR TF@%%p%d\n", i);
+        switch (token->type) {
+        case TOKEN_ID:
+            printf("MOVE TF@%%p%d LF@%s\n", i, token->value->content);
+            break;
+        case TOKEN_INT:
+            printf("MOVE TF@%%p%d int@%s\n", i, token->value->content);
+            break;
+        case TOKEN_NUMBER:
+            printf("MOVE TF@%%p%d float@%s\n", i, token->value->content);
+            break;
+        case TOKEN_STRING:
+            printf("MOVE TF@%%p%d string@%s\n", i, token->value->content);
+            break;
+        default:
+            /* Should not happen */
+            ERR_MSG("Unexpected type of return value: ", token->line);
+            fprintf(stderr, "%d\n", token->type);
+            break;
+        }
+
+        i++;
+        FREE(token);
+    }
+}
+
+void gen_func_call(const char *func_name, tstack_s *in_params)
+{
+    puts("CREATEFRAME");
+    gen_push_arg(in_params);
+    printf("CALL $-%s", func_name);
 }

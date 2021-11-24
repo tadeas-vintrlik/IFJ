184011dc50cafa065aa15c73d344cbe6deaa0f46
tadeas-vintrlik <xvintr04@stud.fit.vutbr.cz>
2021-11-24 10:39:02 +0100

    add basic syntax and semantic tests

--- tests/test_run.sh
+++ tests/test_run.sh
@@ -6,6 +6,13 @@ do
     then
         echo "=== Running $file ==="
         ./"$file"
+        RC="$?"
         echo "=== End of $file ==="
+        if [ "$RC" != 0 ]
+        then
+            exit 1
+        fi
     fi
 done
+
+exit 0
